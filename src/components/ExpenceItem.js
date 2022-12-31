ce606ed10185bbfde79bd65d09199da7981c23d7
Abhishek-data <abhishekkr1915@gmail.com>
2022-12-31 18:02:57 +0530

    Expence Item Comp

---
+++ src/components/ExpenceItem.js
@@ -0,0 +1,15 @@
+function ExpenceItem() {
+   return (
+    <div>
+        <h1>Expence Item</h1>
+        <div>
+            <p>Food Rs 10</p>
+            <p>Petrol Rs 100</p>
+            <p>Movies Rs 200</p>
+        </div>
+    </div>
+   );
+
+}
+
+export default ExpenceItem;
