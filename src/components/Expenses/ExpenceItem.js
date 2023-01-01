8832124b02369a3155b31096f90aa891807324bf
Abhishek-data <abhishekkr1915@gmail.com>
2023-01-01 16:53:14 +0530

    Added button for event handler

--- src/components/Expenses/ExpenceItem.js
+++ src/components/Expenses/ExpenceItem.js
@@ -2,9 +2,23 @@ import React from "react";
 import Card from "../UI/Card";
 import ExpenceDate from "./ExpenceDate";
 import "./ExpenceItem.css";
-
+import { useState } from 'react';
 
 const ExpenceItem = (props)=> {
+  const clickHandler = () =>{
+    console.log('clicked!!!')
+  };
+  const [isActive, setIsActive] = useState(true);
+
+  const handleDelete = (event) => {
+    setIsActive(false);
+  }
+
+  if (!isActive) {
+    return null;
+  }
+
+
   return (
     <Card className="expense-item">
       <ExpenceDate date={props.date}  />
@@ -12,6 +26,8 @@ const ExpenceItem = (props)=> {
       <h2>{props.title}</h2>
       <div className="expense-item__price">${props.amount}</div>
     </div>
+    <button onClick={clickHandler}>Change Title</button>
+    <button onClick={handleDelete}>Delete Expense</button>
     </Card>
   );
 }
