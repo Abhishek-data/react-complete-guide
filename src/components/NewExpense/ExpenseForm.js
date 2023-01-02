f86aa96a797675b093ec3711c1da12b4ffd6d520
Abhishek-data <abhishekkr1915@gmail.com>
2023-01-02 15:28:49 +0530

    Lifting State up fetching data fromchild to parent

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -41,7 +41,7 @@ const ExpenseForm = (props) => {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
-    console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
